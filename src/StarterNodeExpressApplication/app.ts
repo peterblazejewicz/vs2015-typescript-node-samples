62f4f384e994715c8ecc48450e71927e42409bf6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-02-10 23:31:38 +0100

    WIP implementation change

---
+++ src/StarterNodeExpressApplication/app.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+
+import routes = require('./routes/index');
+import * as users from './routes/users';
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+module.exports = app;
